235e8cb25264ccfe2f696f1dcfbbe8dbb0bbb8e4
gisellez143 <giselle114820@mcpsva.org>
2026-02-26 00:34:27 +0000

    Done code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,9 +1,22 @@
 """
 Filename: <number_guessing_game.py>
-Author: <Lastname, Firstname>
-Created: <MM/DD/YYYY>
-Instructor: Holtslander
+Author: <Zavala, Giselle>
+Created: <2/25/2026>
+Instructor: Giselle
 """
 from random import randint
 
 num = randint(1,100)
+
+print("Welcome to the numberb guessing game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    guess = int(input("enter your guess: \n"))
+    if guess < num:
+        print("Too low! try again")
+    elif guess > num:
+        print("Too high! try again")
+    else:
+        print("Correct! you got the number! tanks for playing")
+        break
